f5d1986db5ca22a40cbabdb845bb3eb99c874f2d
McSohan <60097620+McSohan@users.noreply.github.com>
2020-04-05 19:32:54 +0530

    Update client.js
    update link in function

--- app/static/client.js
+++ app/static/client.js
@@ -1,8 +1,8 @@
 var el = x => document.getElementById(x);
 
+var audio;
+
 async function playAudio() {
-  var audio = new Audio('https://file-examples.com/wp-content/uploads/2017/11/file_example_WAV_1MG.wav');  
-  audio.type = 'audio/wav';
   try {
     await audio.play();
     console.log('Playing...');
@@ -41,6 +41,8 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Predicted note denomination outcome =${response["result"]}`;
+      audio = new Audio('https://file-examples.com/wp-content/uploads/2017/11/file_example_WAV_1MG.wav');  
+      audio.type = 'audio/wav';
     }
     el("analyze-button").innerHTML = "Analyze";
   };
