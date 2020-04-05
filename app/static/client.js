c341c5cf37f31cdf5266932b46cf794993c400cb
McSohan <60097620+McSohan@users.noreply.github.com>
2020-04-05 19:57:46 +0530

    Update client.js
    with one button

--- app/static/client.js
+++ app/static/client.js
@@ -2,13 +2,9 @@ var el = x => document.getElementById(x);
 
 var audio;
 
-async function playAudio() {
-  try {
-    await audio.play();
-    console.log('Playing...');
-  } catch (err) {
-    console.log('Failed to play...' + error);
-  }
+function setAudio(url) {
+  audio = new Audio(url);  
+  audio.type = 'audio/wav';
 }
 
 function showPicker() {
@@ -41,10 +37,10 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Predicted note denomination outcome =${response["result"]}`;
-      audio = new Audio('https://file-examples.com/wp-content/uploads/2017/11/file_example_WAV_1MG.wav');  
-      audio.type = 'audio/wav';
+      setAudio('https://file-examples.com/wp-content/uploads/2017/11/file_example_WAV_1MG.wav');
     }
     el("analyze-button").innerHTML = "Analyze";
+    audio.play();
   };
 
   var fileData = new FormData();
